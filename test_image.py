f06e149f21ac4f00838c34b72106256edf5ec376
kinarora <166278115+kinarora@users.noreply.github.com>
2024-04-06 23:57:04 -0700

    Add files via upload

---
+++ test_image.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  5 00:05:54 2022
+
+@author: Chris
+"""
+
+##### turn certificate verification off  #####
+import os, ssl
+if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
+getattr(ssl, '_create_unverified_context', None)):
+    ssl._create_default_https_context = ssl._create_unverified_context
+
+## import libraries
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import pathlib
+import certifi
+
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+
+
+
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck']
+
+def load_image(filename):
+	img = load_img(filename, target_size=(32, 32))
+	img = img_to_array(img)
+	img = img.reshape(1, 32, 32, 3)
+	img = img / 255.0
+	return img
+
+
+# LOAD YOUR OWN TRAINED MODEL 
+model = load_model('TeamSaturn_CIFARmodel_improved.h5')
+
+# get the image from the internet
+URL = "https://wagznwhiskerz.com/wp-content/uploads/2017/10/home-cat.jpg"
+picture_path  = tf.keras.utils.get_file(origin=URL)
+img = load_image(picture_path)
+result = model.predict(img)
+
+# show the picture
+image = plt.imread(picture_path)
+plt.imshow(image)
+
+# show prediction result.
+print('\nPrediction: This image most likely belongs to ' + class_names[int(result.argmax(axis=-1))])
+
+####
+# get the image from the internet
+URL = "https://img.freepik.com/free-photo/airplane-flying-cloudy-sky_144627-132.jpg"
+picture_path  = tf.keras.utils.get_file(origin=URL)
+img = load_image(picture_path)
+result = model.predict(img)
+
+# show the picture
+image = plt.imread(picture_path)
+plt.imshow(image)
+
+# show prediction result.
+print('\nPrediction: This image most likely belongs to ' + class_names[int(result.argmax(axis=-1))])
+
+####
+# get the image from the internet
+URL = "https://img.freepik.com/free-vector/modern-blue-urban-adventure-suv-vehicle-illustration_1344-205.jpg"
+picture_path  = tf.keras.utils.get_file(origin=URL)
+img = load_image(picture_path)
+result = model.predict(img)
+
+# show the picture
+image = plt.imread(picture_path)
+plt.imshow(image)
+
+# show prediction result.
+print('\nPrediction: This image most likely belongs to ' + class_names[int(result.argmax(axis=-1))])
+
+####
+# get the image from the internet
+URL = "https://thumbs.dreamstime.com/z/cruise-ship-1753236.jpg"
+picture_path  = tf.keras.utils.get_file(origin=URL)
+img = load_image(picture_path)
+result = model.predict(img)
+
+# show the picture
+image = plt.imread(picture_path)
+plt.imshow(image)
+
+# show prediction result.
+print('\nPrediction: This image most likely belongs to ' + class_names[int(result.argmax(axis=-1))])
+
+
+####
+# get the image from the internet
+URL = "https://static.toiimg.com/thumb/msid-67586673,width-1070,height-580,overlay-toi_sw,pt-32,y_pad-40,resizemode-75,imgsize-3918697/67586673.jpg"
+picture_path  = tf.keras.utils.get_file(origin=URL)
+img = load_image(picture_path)
+result = model.predict(img)
+
+# show the picture
+image = plt.imread(picture_path)
+plt.imshow(image)
+
+# show prediction result.
+print('\nPrediction: This image most likely belongs to ' + class_names[int(result.argmax(axis=-1))])
+
+####
